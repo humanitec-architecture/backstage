b7912006e01d131d53204fdf75c2044f9f0c242e
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2024-05-10 16:08:13 -0400

    node-service - add MESSAGE and MySQL version/host

--- templates/node-service/content/index.js
+++ templates/node-service/content/index.js
@@ -2,8 +2,9 @@ const http = require('http');
 const mysql = require('mysql2');
 
 // create the connection to database
+const host = process.env.DB_HOST || 'localhost';
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost'  ,
+  host,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'secret',
   database: process.env.DB_DATABASE || 'score',
@@ -13,19 +14,22 @@ const connection = mysql.createConnection({
 const requestHandler = async (request, response) => {
   console.log(request.url);
 
-  // Run hello world query
-  const [rows, fields] = await connection.promise().query('SELECT "This is an example application deployed with Score!" as message');
+  const message = process.env.MESSAGE || "Hello, World!";
 
-  const message = rows[0].message;
+  // Run hello world query
+  const [rows, fields] = await connection.promise().query(
+    'SELECT version() as version'
+  );
+  const version = rows[0].version;
 
   const html = `
   <html>
     <body>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
       <div class="container text-center mt-5 pt-5">
-        <h1>Hello World!</h1>
-        <p>${message}</p>
+      <h1>${message}</h1>
+      <p>This is an application talking to a MySQL <code>${version}</code> database on host <code>${host}</code>, deployed with Score!</p>
       </div>
     </body>
   </html>
